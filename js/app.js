38cb17b7b933ee0f40d399825c989450dfc0f059
Jordan <g0983230881@gmail.com>
2023-04-29 00:38:58 +0800

    canvas2 turn on

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Overlay_Canvas3.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Overlay_Canvas2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
